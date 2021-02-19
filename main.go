b9505a0044299934da02dbc4d1afd76c075358da
weeellz <weeellz12@gmail.com>
2021-02-19 23:42:10 +0300

    Handle outer HTTP requests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -15,6 +16,9 @@ const startCommand string = "/start"
 const telegramApiBaseUrl string = "https://api.telegram.org/bot"
 const telegramApiSendMessage string = "/sendMessage"
 const telegramTokenEnv string = "TELEGRAM_BOT_TOKEN"
+const bytesInMegabyte int64 = 1048576
+
+var chatId int = 0
 
 var telegramApi string = telegramApiBaseUrl + os.Getenv(telegramTokenEnv) + telegramApiSendMessage
 
@@ -32,13 +36,31 @@ type Update struct {
 	Message  Message `json:"message"`
 }
 
-func parseRequest(r *http.Request) (*Update, error) {
+type OuterMessage struct {
+	Text string `json:"text"`
+}
+
+func parseTelegramRequest(body []byte) (*Update, error) {
 	var update Update
-	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
-		log.Printf("Couldn't decode incoming update %s", err.Error())
+	err := json.Unmarshal(body, &update)
+	if err != nil {
+		log.Printf("Couldn't decode incoming update. Error: %s", err.Error())
 		return nil, err
 	}
+	if update.Message.Text == startCommand {
+		chatId = update.Message.Chat.Id
+	}
 	return &update, nil
+}
+
+func parseOuterRequest(body []byte) (*OuterMessage, error) {
+	var message OuterMessage
+	err := json.Unmarshal(body, &message)
+	if err != nil {
+		log.Printf("Couldn't decode incoming outer HTTP request. Error: %s", err.Error())
+		return nil, err
+	}
+	return &message, nil
 }
 
 func sendTextToChat(chatId int, text string) (string, error) {
@@ -58,7 +80,7 @@ func sendTextToChat(chatId int, text string) (string, error) {
 
 	var bodyBytes, errRead = ioutil.ReadAll(response.Body)
 	if errRead != nil {
-		log.Printf("Error in parsing Telegram answer %s", errRead.Error())
+		log.Printf("Error in parsing Telegram answer: %s", errRead.Error())
 		return "", err
 	}
 	bodyString := string(bodyBytes)
@@ -67,20 +89,47 @@ func sendTextToChat(chatId int, text string) (string, error) {
 	return bodyString, nil
 }
 
-func HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
+func HandleOuterHTTPRequest(body []byte) {
+	var outerMessage, errOuter = parseOuterRequest(body)
 
-	var update, err = parseRequest(r)
-	if err != nil {
-		log.Printf("Error parsing update, %s", err.Error())
+	if errOuter != nil {
+		log.Printf("Couldn't parse outer HTTP request. Error: %s", errOuter.Error())
+		return
+	}
+
+	var telegramResponseBody, errTelegram = sendTextToChat(chatId, string(body) /*outerMessage.Text*/)
+	if errTelegram != nil {
+		log.Printf("Got error %s from Telegram, response body is %s", errTelegram.Error(), telegramResponseBody)
+	} else {
+		log.Printf("Message [%s] successfuly distributed to chat id [%d]", outerMessage.Text, chatId)
+	}
+}
+
+func HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
+	requestBodyBytes, errRead := ioutil.ReadAll(io.LimitReader(r.Body, bytesInMegabyte))
+	if errRead != nil {
+		log.Printf("Couldn't read request body. Error: %s", errRead.Error())
+		return
+	}
+
+	var update, errUpdate = parseTelegramRequest(requestBodyBytes)
+
+	if errUpdate != nil {
+		log.Printf("Couldn't parse Telegram update. Error: %s", errUpdate.Error())
+		return
+	}
+
+	if update.Message.Chat.Id == 0 {
+		HandleOuterHTTPRequest(requestBodyBytes)
 		return
 	}
 
 	message := "Hello world!"
 
-	var telegramResponseBody, errTelegram = sendTextToChat(update.Message.Chat.Id, message)
+	var telegramResponseBody, errTelegram = sendTextToChat(chatId, message)
 	if errTelegram != nil {
 		log.Printf("Got error %s from Telegram, response body is %s", errTelegram.Error(), telegramResponseBody)
 	} else {
-		log.Printf("Message [%s] successfuly distributed to chat id [%d]", message, update.Message.Chat.Id)
+		log.Printf("Message [%s] successfuly distributed to chat id [%d]", message, chatId)
 	}
 }
